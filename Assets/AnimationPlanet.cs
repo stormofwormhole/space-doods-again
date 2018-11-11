8034589ced9d6b64cc774b71da9b93656164de9f
stormofwormhole <song.w@outlook.com>
2018-11-11 11:41:09 +0100

    Sonic-Debris03

---
+++ Assets/AnimationPlanet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationPlanet : MonoBehaviour
+{
+
+    public Sprite[] Sprites;
+    public float speed;
+    private SpriteRenderer spriterenderer;
+
+    void Start()
+    {
+        spriterenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    void Update()
+    {
+        int index = (int)(Time.time * speed) % Sprites.Length;
+        spriterenderer.sprite = Sprites[index];
+    }
+}
