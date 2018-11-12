b0f79fca26ba3ed7c15bb01cd4a9b7548f8e4f3c
Laura Uhliarova <lau.uhl@gmail.com>
2018-11-13 00:32:42 +0100

    new Scenes

---
+++ Assets/Scripts/Laura/ReloadScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadScript : MonoBehaviour {
+
+    public void Sceneload(){
+
+        SceneManager.LoadScene("FINAL2");
+		
+	}
+}
