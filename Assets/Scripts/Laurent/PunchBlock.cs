7b4aea865fe6e475801dc122e35f5407f19e91ce
laurent gehin <laurentgehin@hotmail.com>
2018-11-12 09:27:28 +0100

    Animated Characters_Laurent

---
+++ Assets/Scripts/Laurent/PunchBlock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunchBlock : MonoBehaviour {
+
+private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("z"))
+		{
+			animator.SetBool(("IsDefending"), true);
+
+		}
+
+		 else animator.SetBool("IsDefending", false);
+
+	}
+}
